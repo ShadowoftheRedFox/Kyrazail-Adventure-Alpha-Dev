c77acc5b73ba36ef51d16c1c33ad5fd35698db2d
Hugo LM <leon.merin@gmail.com>
2022-08-03 13:50:27 +0200

    added data load

---
+++ src/plugin/plugin.data.js
@@ -0,0 +1,24 @@
+const StackLoadData = [{
+    name: "Data",
+    path: "./resources/data/",
+    status: true,
+    parameters: {},
+    description: "Data needed for the game.",
+    objPath: ""
+},
+{
+    name: "package",
+    path: "./",
+    status: true,
+    parameters: {},
+    description: "Game ID and infos for updates.",
+    objPath: ""
+},
+{
+    name: "Update",
+    path: "./resources/data/",
+    status: true,
+    parameters: {},
+    description: "Update information on last update check.",
+    objPath: ""
+}];
