659dc9b7a2050770c3a620dbae87e493708783b3
Hugo LM <leon.merin@gmail.com>
2022-08-03 12:44:04 +0200

    added loadgame

--- src/src.main.js
+++ src/src.main.js
@@ -1,1 +1,17 @@
 document.ondrag = document.ondragstart = document.oncontextmenu = function () { return false; };
+
+var GameAudiosToLoad = [];
+var GameImagesToLoad = [];
+
+ScriptLoaderManager.setup(StackLoadPlugin, 0, () => {
+    ScriptLoaderManager.setup(StackLoadBuild.concat(
+        StackLoadClass, StackLoadConfig, StackLoadCore,
+        StackLoadEntity, StackLoadEvent, StackLoadFunction,
+        StackLoadGlobal, StackLoadInterface, StackLoadManager,
+        StackLoadUtil
+    ), 0, () => {
+        DataLoaderManager.setup(StackLoadData, 0, () => {
+
+        });
+    });
+});
