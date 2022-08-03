9b674a165d159b79f6a5759cdb4af59df1ace4e4
Hugo LM <leon.merin@gmail.com>
2022-08-03 13:45:15 +0200

    creation/pre update

---
+++ src/config/config.const.js
@@ -0,0 +1,30 @@
+const ConfigConst = {
+    KEY: "9z$C&E)H@McQfTjWnZr4u7x!A%D*G-JaNdRgUkXp2s5v8y/B?E(H+MbPeShVmYq3t6w9z$C&F)J@NcRfTjWnZr4u7x!A%D*G-KaPdSgVkXp2s5v8y/B?E(H+MbQeThWmZq3t6w9z$C&F)J@NcRfUjXn2r5u7x!A%D*G-KaPdSgVkYp3s6v9y/B?E(H+MbQeThWmZq4t7w!z%C&F)J@NcRfUjXn2r5u8x/A?D(G-KaPdSgVkYp3s6v9y$B&E)H@MbQeThWmZq4t7w!z%C*F-JaNdRfUjXn2r5u8x/A?D(G+KbPeShVkYp3s6v9y$B&E)H@McQfTjWnZq4t7w!z%C*F-JaNdRgUkXp2s5u8x/A?D(G+KbPeShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2s5v8y/B?E(G+KbPeShVmYq3t6w9z$C&F)J@McQfTjWnZr4u7x!A%D*G-KaPdRgUkXp2s5v8y/B?E(H+MbQeThVmYq3t6",
+    DISCORD: "https://discord.gg/5mF5AHnRCr",
+    GITHUB: "https://github.com/ShadowoftheRedFox/Kyrazail-Adventure-Dev.git",
+    LANGUAGE: "en",
+    ZINDEX: {
+        MAP: 0,
+        MAPANIMATED: 1,
+        ENTITIES: 2,
+        MAPOVER: 3,
+        MAPEFFECT: 4,
+        FIGHT: 5,
+        PLAYERQUIPEMENT: 6,
+        PAUSE: 7,
+        LOADING: 999,
+        ERROR: 1000
+    },
+    TIP: [
+        "Why is mana blue?",
+        "Found a bug? Check out our Discord to report it!",
+        "Stronger monsters give you better loots!",
+        "Bosses can give you special items!",
+        "Search around the map, maybe you will find something interesting...",
+        "The most expensive equipement is not always the best...",
+        "Use potions if the fight is to tough."
+    ],
+    CONTAINER: (function () { return document.getElementById("container"); })(),
+    MAINCONTAINER: (function () { return document.getElementById("mainContainer"); })(),
+    DEBUG: false
+};
---
+++ src/plugin/stack/stack.build.js
@@ -0,0 +1,1 @@
+const StackLoadBuild = [];
