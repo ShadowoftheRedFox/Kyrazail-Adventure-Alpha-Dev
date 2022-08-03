6be04bf0f33fff4fed78935ffe8275fbfcf655ae
Hugo LM <leon.merin@gmail.com>
2022-08-03 22:24:08 +0200

    added all current possible language

--- ts/type.d.ts
+++ ts/type.d.ts
@@ -4,7 +4,7 @@ declare global {
         KEY: "9z$C&E)H@McQfTjWnZr4u7x!A%D*G-JaNdRgUkXp2s5v8y/B?E(H+MbPeShVmYq3t6w9z$C&F)J@NcRfTjWnZr4u7x!A%D*G-KaPdSgVkXp2s5v8y/B?E(H+MbQeThWmZq3t6w9z$C&F)J@NcRfUjXn2r5u7x!A%D*G-KaPdSgVkYp3s6v9y/B?E(H+MbQeThWmZq4t7w!z%C&F)J@NcRfUjXn2r5u8x/A?D(G-KaPdSgVkYp3s6v9y$B&E)H@MbQeThWmZq4t7w!z%C*F-JaNdRfUjXn2r5u8x/A?D(G+KbPeShVkYp3s6v9y$B&E)H@McQfTjWnZq4t7w!z%C*F-JaNdRgUkXp2s5u8x/A?D(G+KbPeShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2s5v8y/B?E(G+KbPeShVmYq3t6w9z$C&F)J@McQfTjWnZr4u7x!A%D*G-KaPdRgUkXp2s5v8y/B?E(H+MbQeThVmYq3t6",
         DISCORD: "https://discord.gg/5mF5AHnRCr",
         GITHUB: "https://github.com/ShadowoftheRedFox/Kyrazail-Adventure-Dev.git",
-        LANGUAGE: "en",
+        LANGUAGE: "en" | "fr",
         ZINDEX: {
             MAP: 0,
             MAPANIMATED: 1,
@@ -22,7 +22,7 @@ declare global {
         MAINCONTAINER: HTMLElement | null,
         DEBUG: boolean
     };
-    
+
     var GameAudiosToLoad: [];
     var GameImagesToLoad: [];
 }
