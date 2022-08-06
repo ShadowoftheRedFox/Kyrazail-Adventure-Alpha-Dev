39770392f0c8fe8c6dd692988c1e520f54304a1c
Hugo LM <leon.merin@gmail.com>
2022-08-06 21:03:48 +0200

    updated types
    added loading screen manager to the type
    added:
    -game entities class options
    -game status effect
    -game items
    -game recipe
    -game attack type
    -game orientation
    -encoded message
    -game item type
    -remove duplicate function
    -game entities stats
    -game special ability

--- ts/type.d.ts
+++ ts/type.d.ts
@@ -47,7 +47,210 @@ declare global {
         }
     };
 
+    const LoadingScreenManager: {
+        w: number;
+        h: number;
+
+        progress: number;
+        interval: number;
+        progressMax: number;
+        progressLast: number;
+        refreshSpeed: number;
+        progressSmooth: number;
+        progressAnimation: number;
+        message: string;
+
+        ctx: CanvasRenderingContext2D | null;
+        viewport: HTMLCanvasElement | null;
+
+        trailingStep: number;
+        trailingCount: number;
+        trailingSpeed: number;
+
+        tipIndex: number;
+        tipCount: number;
+        tipSpeed: number;
+
+        stripeStep: number;
+        stripeSpeed: number;
+        stripeAlpha: number;
+        stripeColor: string;
+
+        animationW: number;
+        animationH: number;
+        animationStep: number;
+        animationImage: null;
+        animationMargin: number;
+        animationLastStep: number;
+        animationStepCount: number;
+        animationStepSpeed: number;
+        animationStepSpeedDistance: number;
+        animationPositionSpeed(w: number): number
+        animationPosition: number;
+        calledEqual: false;
+
+        init(callWhenEqual: () => void): void
+        end(): void
+        edit(): void
+        bar(): void
+        animate(): void
+        tip(): void
+        title(): void
+        addProgress(n: number): void
+        setMaxProgress(n: number): void
+        createPattern(color: string | "grey", alpha: number | 1): CanvasPattern
+        progressFunction(): void
+    };
+
     var GameAudiosToLoad: [];
     var GameImagesToLoad: [];
-}
 
+    type GameEntitiesOptions = {
+        name: string | { en: string, fr: string };
+        /** If type is "hostile".*/
+        stats: GameEntitiesStats
+        /** If type is marchand.*/
+        shop: {
+            canPlayerSell: boolean,
+            canPlayerBuy: boolean,
+            canPlayerEquip: boolean,
+            items: GameItem[],
+            welcomeMessage: GameLanguageCodedString[]
+        };
+        /** If type is npc.*/
+        dialog: GameLanguageCodedString[];
+
+        type: "hostile" | "npc" | "marchand" | "player";
+        pattern: "follow" | "merge" | "idle" | "custom" | "player";
+
+        spawnX: number;
+        spawnY: number;
+        spawnOrientation: GameOrientation;
+
+        character: {
+            invisible: boolean,
+            image: string,
+            col: number,
+            row: number
+        };
+
+        movementSpeed: number;
+
+        speakImage: string;
+    };
+
+    type GameStatusEffect = {
+        /** In round.*/
+        duration: number,
+        /** Add the number for the target on his pv.*/
+        pv: number,
+        /** Add the number for the target on his mp.*/
+        mp: number,
+        /** Add the number for the target on his sp.*/
+        sp: number,
+        /** If the effect can cause death.*/
+        canKill: boolean,
+        /** The image displayed in the status.*/
+        effectImage: string,
+        effectRow: number,
+        effectCol: number,
+        /** If there is an animation each time the effect is inflicted.*/
+        animationEffect: boolean,
+        animationImage: string
+    };
+
+    type GameItem = {
+        name: { fr: string, en: string },
+        image: string,
+        col: number,
+        rown: number,
+        description: GameLanguageCodedString,
+        special: boolean,
+        usable: boolean,
+        /** How many times can we use this item.*/
+        usageAmount: number,
+        helmet: boolean,
+        torso: boolean,
+        legging: boolean,
+        boot: boolean,
+        singleHanded: boolean,
+        twoHanded: boolean,
+        type: GameItemType[]
+    };
+
+    type GameRecipe = {
+        name: { fr: string, en: string },
+        description: GameLanguageCodedString,
+        /** Array of items names */
+        recipeList: {
+            /** Refers to an item name.*/
+            item: string,
+            amount: number
+        }[],
+        usageAmount: number | null,
+        result: {
+            /** Refers to an item name.*/
+            item: string,
+            amount: number
+        }
+    }
+
+    type GameAttackType = "physical" | "magic" | "fire" | "water" | "ice" | "electricity" | "dark" | "holy";
+
+    type GameOrientation = "east" | "south" | "west" | "north";
+
+    /** A code that will be used in the language module to return the true string but translated in the wanted language.*/
+    type GameLanguageCodedString = string;
+
+    type GameItemType = "resource" | "equipement" | "potion";
+
+
+    /**
+     * Remove any duplicate from the array, so it has every item once.
+     */
+    function RemoveDuplicate(a: any[]): any[];
+
+    type GameEntitiesStats = {
+        pv: number,
+        mp: number,
+        sp: number,
+
+        def: number,
+        magicdef: number
+        atk: number,
+        magicatk: number
+        agi: number,
+        luck: number,
+
+        special: GameSpecialAbility[],
+
+        /** Regenerate by himself.*/
+        regeneration: number,
+
+        /** Reduce by 2 the damage taken.*/
+        resistance: GameAttackType[],
+        /** Increase by 2 the damage taken.*/
+        weakness: GameAttackType[],
+
+        status: GameStatusEffect[],
+        loots: GameItem[],
+        gold: number,
+        exp: number,
+
+        boss: boolean,
+        bossLoot: GameItem[]
+    };
+
+    type GameSpecialAbility = {
+        name: { fr: string, en: string },
+        mpCost: number,
+        hpCost: number,
+        spCost: number,
+        animationImage: string | null,
+        type: GameAttackType,
+        /** Delay before using the skill again, in rounds.*/
+        delay: number,
+        /** If there is a max amount of user per battle.*/
+        usePerBattle: number | null
+    };
+}
