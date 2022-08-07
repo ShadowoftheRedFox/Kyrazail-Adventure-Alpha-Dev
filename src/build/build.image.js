7adbb9c3ec0004b27bded1df6f55545d526e9784
Hugo LM <leon.merin@gmail.com>
2022-08-07 12:37:11 +0200

    added image structure
    give the amount of row and col per image, as well as the tile size
    if image is not tiled, just the image is specified

--- src/build/build.image.js
+++ src/build/build.image.js
@@ -1,31 +1,36 @@
 /// <reference path="../../ts/type.d.ts"/>
 
 /**
- * 
+ * @param {GameScope} scope
  * @param {string[]} imageArray 
  * @param {()=>{}} callback 
  */
-function GameLoadImage(imageArray, callback) {
+function GameLoadImage(scope, imageArray, callback) {
     if (!imageArray) throw new ReferenceError("You must pass an array.");
     if (!Array.isArray(imageArray)) throw new TypeError("The imageArray must be a string array.");
     if (callback && typeof callback != "function") throw new TypeError("Callback must be a function");
+
+    LoadingScreenManager.message = "Loading images";
+    LoadingScreenManager.setMaxProgress(imageArray.length);
 
     if (imageArray.length == 0) return callback();
     var imagesLeftToLoad = imageArray.length;
 
     imageArray.forEach(image => {
-        if (window.game.cache.image[image]) console.log(`${image} is already loaded`);
+        if (scope.cache.image[image]) console.log(`${image} is already loaded`);
         else {
             const i = new Image();
             i.onerror = function () {
                 imagesLeftToLoad--;
+                LoadingScreenManager.addProgress(1);
                 console.warn(`${i.src} failed`);
             };
             i.onload = function () {
                 imagesLeftToLoad--;
-                window.game.cache.image[image] = GameLoadImage.structure(i, image);
+                LoadingScreenManager.addProgress(1);
+                scope.cache.image[image] = GameLoadImage.structure(i, image);
             };
-            i.src = window.game.constants.href + "/" + image + ".png";
+            i.src = scope.constants.href + "resources/Image/" + image + ".png";
         }
     });
 }
@@ -39,4 +44,63 @@ function GameLoadImage(imageArray, callback) {
 GameLoadImage.structure = function (image, n) {
     const folder = n.split("/")[0],
         name = n.split("/")[1];
+
+    const s = {
+        image: image
+    };
+
+    if (["Battlebacks1", "Battlebacks2", "Battlers", "Parallaxes", "Titles1", "Titles2", "Intro"].includes(folder)) return s;
+    if (folder === "Tilesets") {
+        s.tileW = 16;
+        s.tileH = 16;
+        s.col = image.width / 16;
+        s.row = image.height / 16;
+        return s;
+    }
+    if (folder === "Animations") {
+        s.tileW = 192;
+        s.tileH = 192;
+        s.col = image.width / 192;
+        s.row = image.height / 192;
+        return s;
+    }
+    if (folder === "Faces") {
+        s.tileW = 96;
+        s.tileH = 96;
+        s.col = image.width / 96;
+        s.row = image.height / 96;
+        return s;
+    }
+    if (folder === "System") {
+        if (name === "Shadow" || name === "Window" || name === "GameOver" || name === "BattleStart") return s;
+        if (name === "Balloon") {
+            s.tileW = 32;
+            s.tileH = 32;
+            s.col = image.width / 32;
+            s.row = image.height / 32;
+            return s;
+        }
+        if (name === "IconSet") {
+            s.tileW = 16;
+            s.tileH = 16;
+            s.col = image.width / 16;
+            s.row = image.height / 16;
+            return s;
+        }
+    }
+    if (folder === "Characters") {
+        if (name.includes("$")) {
+            s.tileW = image.width / 3;
+            s.tileH = image.height / 4;
+            s.col = 3;
+            s.row = 4;
+            return s;
+        } else {
+            s.tileW = image.width / 12;
+            s.tileH = image.height / 8;
+            s.col = 12;
+            s.row = 8;
+            return s;
+        }
+    }
 };
