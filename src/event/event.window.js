7625534b4304f29e9dd7a0ffbfa29af8d4490167
Hugo LM <leon.merin@gmail.com>
2022-08-09 20:04:50 +0200

    added the on resize event handler

--- src/event/event.window.js
+++ src/event/event.window.js
@@ -1,1 +1,9 @@
 /// <reference path="../../ts/type.d.ts"/>
+
+document.onfullscreenchange = window.onresize = resize;
+
+function resize() {
+    const $ = ConfigConst.MAINCONTAINER;
+    regenerateAllCanvas($.offsetWidth, $.offsetHeight);
+    console.log("a");
+}
