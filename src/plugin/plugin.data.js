c775c4db01de2d06e8ac05d11f0e8f94ca6c067b
Hugo LM <leon.merin@gmail.com>
2022-08-09 19:59:58 +0200

    added the test map into the loaed json

--- src/plugin/plugin.data.js
+++ src/plugin/plugin.data.js
@@ -5,20 +5,25 @@ const StackLoadData = [{
     parameters: {},
     description: "Data needed for the game.",
     objPath: ""
-},
-{
+}, {
     name: "package",
     path: "./",
     status: true,
     parameters: {},
     description: "Game ID and infos for updates.",
     objPath: ""
-},
-{
+}, {
     name: "Update",
     path: "./resources/data/",
     status: true,
     parameters: {},
     description: "Update information on last update check.",
     objPath: ""
+}, {
+    name: "test",
+    path: "./resources/data/map/",
+    status: true,
+    parameters: {},
+    description: "Test map.",
+    objPath: "map"
 }];
