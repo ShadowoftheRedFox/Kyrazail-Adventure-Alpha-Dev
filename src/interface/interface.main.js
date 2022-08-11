c3364de652dceac87ba4925ecd4449ef620d5cc0
Hugo LM <leon.merin@gmail.com>
2022-08-11 20:36:40 +0200

    Update interface.main.js

--- src/interface/interface.main.js
+++ src/interface/interface.main.js
@@ -1,1 +1,39 @@
 /// <reference path="../../ts/type.d.ts"/>
+class GameMainInterface extends GameInterfaces {
+    constructor(scope) {
+        super({
+            asOwnCanvas: true,
+            zindex: ConfigConst.ZINDEX.MAIN,
+            canvasGroup: "GameMainGroup",
+            requiredImage: ["Titles1/Landscape"],
+            requiredAudio: ["MAIN/Adeste", "MAIN/Dramatic", "MAIN/Moon", "MAIN/Silence"],
+            transitionLeave: true,
+            transitionSpawns: true,
+            activated: false,
+            needsUpdate: true
+        }, scope);
+    }
+
+    /**
+     * @param {GameScope} scope 
+     */
+    render(scope) {
+        const ctx = scope.cache.context[this.canvasGroup];
+        var w = scope.w,
+            h = scope.h;
+
+        ctx.clearRect(0, 0, w, h);
+
+        ctx.fillStyle = "#fff";
+        ctx.font = "32px Unreadable";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        ctx.fillText("Main aozidbaz azdoi ahzdo", w / 2, h / 2, w);
+        this.needsUpdate = false;
+    }
+
+    /**
+     * @param {GameScope} scope 
+     */
+    update(scope) { }
+}
