9ca4015ddc45d071b5e200344e07f91a911ea807
Hugo LM <leon.merin@gmail.com>
2022-08-12 15:38:32 +0200

    added example for args

--- src/language/language.translate.js
+++ src/language/language.translate.js
@@ -4,7 +4,7 @@
  * @param {string} messageCode The code message to translate.
  * @param  {...any} [args] The arguments if needed.
  * @returns {string} The translated message.
- * @example GameTranslate("CheckPlayerNameCorrect", "Kyra")
+ * @example GameTranslate("ExamplePlayerNameCheck", "Kyra")
  * // if the game is in language "en":
  * => "Your name is Kyra, is that correct?"
  */
@@ -24,6 +24,8 @@ GameTranslate.toEN = function (m, a) {
         case "WindowManagerFatalError": return "Fatal Error";
         case "WindowManagerLongLog": return "Long crash log found. Check console for a full report.";
         case "EventBeforeUnload": return "Are you sure you want to leave this page? Progression might not be saved.";
+        //? example of how to use args
+        case "ExamplePlayerNameCheck": return `So your name is ${a[0]} ${a[1]}, is that correct?`;
         default: return "Unknown message code";
     }
 };
@@ -33,6 +35,8 @@ GameTranslate.toFR = function (m, a) {
         case "WindowManagerFatalError": return "Erreur Fatale";
         case "WindowManagerLongLog": return "Log de crash long trouvé. Regardez la console pour un rapport complet.";
         case "EventBeforeUnload": return "Êtes vous sur de vouloir quitter la page? La progression n'est peut être pas sauvegardé.";
+        //? exemple de comment utiliser les arguments
+        case "ExamplePlayerNameCheck": return `Donc votre nom est ${a[0]} ${a[1]}, cela est il correct?`;
         default: return "Code de message inconnu";
     }
 };
