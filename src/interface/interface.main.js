de5bdd720b63e27497aa07a22f1b3d0bacda8398
Hugo LM <leon.merin@gmail.com>
2022-08-17 12:01:58 +0200

    begin main menu

--- src/interface/interface.main.js
+++ src/interface/interface.main.js
@@ -1,11 +1,14 @@
 /// <reference path="../../ts/type.d.ts"/>
 class GameMainInterface extends GameInterfaces {
+    /**
+     * @param {GameScope} scope 
+     */
     constructor(scope) {
         super({
             asOwnCanvas: true,
             zindex: ConfigConst.ZINDEX.MAIN,
             canvasGroup: "GameMainGroup",
-            requiredImage: ["Titles1/Landscape"],
+            requiredImage: [],
             requiredAudio: ["MAIN/Adeste", "MAIN/Dramatic", "MAIN/Moon", "MAIN/Silence"],
             transitionLeave: true,
             transitionSpawns: true,
@@ -17,23 +20,52 @@ class GameMainInterface extends GameInterfaces {
     /**
      * @param {GameScope} scope 
      */
-    render(scope) {
-        const ctx = scope.cache.context[this.canvasGroup];
-        var w = scope.w,
-            h = scope.h;
+    startNewGame(scope) { }
+    /**
+     * @param {GameScope} scope 
+     */
+    newGameNo(scope) { }
 
+    /**
+     * @param {GameScope} scope 
+     */
+    render(scope) {
+        const ctx = scope.cache.context[this.canvasGroup],
+            w = scope.w,
+            h = scope.h;
         ctx.clearRect(0, 0, w, h);
 
+        //TODO add social network button (git and discord)
         ctx.fillStyle = "#fff";
-        ctx.font = "32px Unreadable";
+        ctx.font = "32px Azure";
         ctx.textBaseline = "middle";
+        ctx.imageSmoothingQuality = "high";
         ctx.textAlign = "center";
-        ctx.fillText("Main aozidbaz azdoi ahzdo", w / 2, h / 2, w);
+
+        //background title
+        ctx.globalAlpha = 0.6;
+        RectangleCreator.roundRect(ctx, (w - 590) / 2, (h - 100) / 3, 590, 100, 50, true, false);
+        ctx.globalAlpha = 1;
+
+        //create the gradient of title
+        var gradient = ctx.createLinearGradient(w / 2 - 200, h / 3, w / 2 + 200, h / 3);
+        gradient.addColorStop(0, "#F3C126");
+        gradient.addColorStop(0.5, "#6FE0E1");
+        gradient.addColorStop(1, "#3C1EEE");
+        ctx.fillStyle = gradient;
+
+        //write title
+        ctx.textAlign = "center";
+        ctx.font = '40px Azure';
+        ctx.fillText("Kyrazail Adventure", w / 2, h / 3);
+        underline(ctx, "Kyrazail Adventure", w / 2, h / 3, gradient, "40px", ctx.textAlign);
+
+        // ctx.fillText("Main", w / 2, h / 2, w);
         this.needsUpdate = false;
     }
 
     /**
      * @param {GameScope} scope 
      */
-    update(scope) { }
+    update(scope) { return scope.state; }
 }
