fa0731f44299ce5d78c10e753af53c23fa43d51e
Hugo LM <leon.merin@gmail.com>
2022-08-17 16:25:39 +0200

    Update config.main.js

--- src/config/config.main.js
+++ src/config/config.main.js
@@ -10,26 +10,20 @@
  *                                                                          *
  ****************************************************************************/
 const GameConfig = {
-    //if the game will update at the next start
     willUpdate: false,
-    //the fps the game will be running
     targetFps: 60,
-    //always run enabled or disabled
     alwaysRun: false,
-    //key input
     keyBoard: {
-        //? See all KEY NAME here: https://www.freecodecamp.org/news/javascript-keycode-list-keypress-event-key-codes/#a-full-list-of-key-event-values
-        //! It is not recommended to put same keys at two different functionnality
         up: ["ArrowUp", "z"],
         down: ["ArrowDown", "s"],
         right: ["ArrowRight", "d"],
         left: ["ArrowLeft", "q"],
         run: ["Shift"],
-        interaction: ["e", "Enter"], //in game, with the player
+        interaction: ["e", "Enter"],
         debug: ["k"],
         pause: ["p"],
         back: ["Backspace", "x"],
-        confirm: ["Enter"], //in menu
+        confirm: ["Enter"],
         inventory: ["c", "a"]
     }
 };
